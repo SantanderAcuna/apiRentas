70e551fb28aedf4e9be4ba48d26adde52210320f
Jose acuña <panson@misena.edu.co>
2024-11-01 19:49:45 -0500

    Coreecion de lectura en ls controladores

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -10,92 +10,96 @@ use Illuminate\Support\Facades\Hash;
 
 class ApiController extends Controller
 {
-   // Register API (POST, formdata)
-   public function register(Request $request){
+    // Register API (POST, formdata)
+    public function register(Request $request)
+    {
 
-    // data validation
-    $request->validate([
-        "numero_cedula" => "required",
-        "nombre_completo" => "required",
-        "password" => "required",
-        "area" => "required",
-        "es_lider_area" => "required",
-        "es_director" => "required",
-        "email" => "required|email|unique:users",
-        "password" => "required|confirmed"
-    ]);
+        // data validation
+        $request->validate([
+            "numero_cedula" => "required",
+            "nombre_completo" => "required",
+            "password" => "required",
+            "area" => "required",
+            "es_lider_area" => "required",
+            "es_director" => "required",
+            "email" => "required|email|unique:users",
+            "password" => "required|confirmed"
+        ]);
 
-    // Author model
-    User::create([
-        "nombre_completo" => $request->nombre_completo,
-        "numero_cedula" => $request->numero_cedula,
-        "email" => $request->email,
-        "password" => Hash::make($request->password),
-        "area" => $request->area,
-        "es_lider_area" => $request->es_lider_area,
-        "es_director" => $request->es_director,
-       
-    ]);
+        // Author model
+        User::create([
+            "nombre_completo" => $request->nombre_completo,
+            "numero_cedula" => $request->numero_cedula,
+            "email" => $request->email,
+            "password" => Hash::make($request->password),
+            "area" => $request->area,
+            "es_lider_area" => $request->es_lider_area,
+            "es_director" => $request->es_director,
 
-    // Response
-    return response()->json([
-        "status" => true,
-        "message" => "User created successfully"
-    ]);
-}
+        ]);
 
-// Login API (POST, formdata)
-public function login(Request $request){
-
-    // Data validation
-    $request->validate([
-        "email" => "required|email",
-        "password" => "required"
-    ]);
-
-    // Auth Facade
-    if(Auth::attempt([
-        "email" => $request->email,
-        "password" => $request->password
-    ])){
-
-        $user = Auth::user();
-
-        $token = $user->createToken("myToken")->accessToken;
-
+        // Response
         return response()->json([
             "status" => true,
-            "message" => "Login successful",
-            "access_token" => $token
+            "message" => "User created successfully"
         ]);
     }
 
-    return response()->json([
-        "status" => false,
-        "message" => "Invalid credentials"
-    ]);
-}
+    // Login API (POST, formdata)
+    public function login(Request $request)
+    {
 
-// Profile API (GET)
-public function profile(){
-    
-    $userdata = Auth::user();
+        // Data validation
+        $request->validate([
+            "email" => "required|email",
+            "password" => "required"
+        ]);
 
-    return response()->json([
-        "status" => true,
-        "message" => "Profile data",
-        "data" => $userdata
-    ]);
-}
+        // Auth Facade
+        if (Auth::attempt([
+            "email" => $request->email,
+            "password" => $request->password
+        ])) {
 
-// Logout API (GET)
-public function logout(){
+            $user = Auth::user();
 
-    auth()->user()->token()->revoke();
+            $token = $user->createToken("myToken")->accessToken;
 
-    return response()->json([
-        "status" => true,
-        "message" => "User logged out"
-    ]);
-}
+            return response()->json([
+                "status" => true,
+                "message" => "Login successful",
+                "access_token" => $token
+            ]);
+        }
+
+        return response()->json([
+            "status" => false,
+            "message" => "Invalid credentials"
+        ]);
+    }
+
+    // Profile API (GET)
+    public function profile()
+    {
+
+        $userdata = Auth::user();
+
+        return response()->json([
+            "status" => true,
+            "message" => "Profile data",
+            "data" => $userdata
+        ]);
+    }
+
+    // Logout API (GET)
+    public function logout()
+    {
+
+        auth()->user()->token()->revoke();
+
+        return response()->json([
+            "status" => true,
+            "message" => "User logged out"
+        ]);
+    }
 }
